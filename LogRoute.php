9d129e7ecb06ede3455342ca89bb41e18e11c1f9
pcmnac <pcmnac@gmail.com>
2018-12-14 14:09:51 -0300

    added log source (server ip) and changed logtime type to UTCDateTime

--- LogRoute.php
+++ LogRoute.php
@@ -4,6 +4,7 @@ namespace pcmnac\mongoyii;
 
 use Yii;
 use CLogRoute;
+use MongoDB\BSON\UTCDateTime;
 
 /**
 * EMongoLogRoute extends CLogRoute and provides logging
@@ -13,45 +14,46 @@ use CLogRoute;
 
 class LogRoute extends CLogRoute
 {
-	/**
-	 * @var string the connectionId of the EMongoClient component
-	 */
-	public $connectionId = 'mongodb';
+    /**
+     * @var string the connectionId of the EMongoClient component
+     */
+    public $connectionId = 'mongodb';
 
-	/**
-	 * Name of the collection the logs should be stored to.
-	 * @var string
-	 */
-	public $logCollectionName = 'YiiLog';
-	
-	/**
-	 * Get a MongoCollection object
-	 * @return MongoCollection - Instance of MongoCollection
-	 */
-	public function getMongoConnection()
-	{
-		return Yii::app()
-			->{$this->connectionId}
-			->selectDatabase()
-			->{$this->logCollectionName};
-	}
+    /**
+     * Name of the collection the logs should be stored to.
+     * @var string
+     */
+    public $logCollectionName = 'YiiLog';
+    
+    /**
+     * Get a MongoCollection object
+     * @return MongoCollection - Instance of MongoCollection
+     */
+    public function getMongoConnection()
+    {
+        return Yii::app()
+            ->{$this->connectionId}
+            ->selectDatabase()
+            ->{$this->logCollectionName};
+    }
 
-	/**
-	 * Stores log messages into database.
-	 * @param array $logs list of log messages
-	 */
-	public function processLogs($logs)
-	{
-		$collection = $this->getMongoConnection();
-		foreach($logs as $log){
-			$collection->insertOne(
-				array(
-					'level' => $log[1],
-					'category' => $log[2],
-					'logtime' => (int)$log[3],
-					'message' => $log[0],
-				)
-			);
-		}
-	}
+    /**
+     * Stores log messages into database.
+     * @param array $logs list of log messages
+     */
+    public function processLogs($logs)
+    {
+        $collection = $this->getMongoConnection();
+        foreach($logs as $log){
+            $collection->insertOne(
+                array(
+                    'source' => $_SERVER['SERVER_ADDR'],
+                    'level' => $log[1],
+                    'category' => $log[2],
+                    'logtime' => new UTCDateTime((int)$log[3]),
+                    'message' => $log[0],
+                )
+            );
+        }
+    }
 }
